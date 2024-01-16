d83596d5e18871ad03d5017c4bbad86682943eca
Wonni23 <jsus88@naver.com>
2024-01-17 08:48:28 +0900

    Create 2439_별 찍기 - 2.c

---
+++ C/Bronze 4/2439_별 찍기 - 2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    int i;
+    int j;
+
+    i = 1;
+    scanf("%d", &N);
+    while (i <= N)
+    {
+        j = 0;
+        while (j < N)
+        {
+            if (j >= N - i)
+                printf("*");
+            else
+                printf(" ");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
+
+/*
+    i를 습관적으로 0으로 초기화하는 바람에 출력형식이 잘못됐었다.
+    0으로 초기화 할 경우 첫 줄은 공백만 자리하게 되어서 틀리다.
+    문제를 꼼꼼히 잘 확인하자!
+*/
